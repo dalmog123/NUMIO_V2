055f813eb691df6b4b89ca94a1aa77fcb368fe7e
YuvalChen1 <pinewater700@gmail.com>
2025-06-23 20:45:22 +0300

    lag test3

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body >{children}
-        <MouseMoveEffect />
+        {/* <MouseMoveEffect /> */}
       </body>
     </html>
   )
