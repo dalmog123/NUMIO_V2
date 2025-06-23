2706b8299c551c6cde71b96464a61f6839a61b26
YuvalChen1 <pinewater700@gmail.com>
2025-06-23 20:14:54 +0300

    visual changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import MouseMoveEffect from '@/components/MouseMoveEffect'
 
 export const metadata: Metadata = {
   title: 'NUMIO Solutions',
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body >{children}
+        <MouseMoveEffect />
+      </body>
     </html>
   )
 }
